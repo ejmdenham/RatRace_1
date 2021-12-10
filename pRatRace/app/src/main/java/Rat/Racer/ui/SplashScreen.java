ab693b3abe9ef3bfd6550fe5f25011bf66278e1b
ejmdenham <ejmdenham@gmail.com>
2021-12-10 19:53:15 +0100

    Added Splash Screen
    Splash screen with animation which leads to main activity

---
+++ pRatRace/app/src/main/java/Rat/Racer/ui/SplashScreen.java
@@ -0,0 +1,60 @@
+package Rat.Racer.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import Rat.Racer.MainActivity;
+import Rat.Racer.R;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5500;
+
+    //Variables
+    Animation topAnim, bottomAnim;
+    ImageView logo;
+    TextView title, tag;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Animations
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_anim);
+
+        //Hooks
+        logo = findViewById(R.id.imageView2);
+        title = findViewById(R.id.textView2);
+        tag = findViewById(R.id.textView3);
+
+        //init anim
+        logo.setAnimation(topAnim);
+        title.setAnimation(bottomAnim);
+        tag.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Going from splash to Main. the next screen will likely be the login
+                //TODO: implement login screen
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+
+    }
+}
