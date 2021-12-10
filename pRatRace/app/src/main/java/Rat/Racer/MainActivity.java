8622e1731f8cb893cdebc08d737004e5ad7124c7
ejmdenham <ejmdenham@gmail.com>
2021-12-10 11:06:25 +0100

    Re:commit
    App functioning. Added comments for clarity.
    
    Recommended next steps: Prototyping on Figma

--- pRatRace/app/src/main/java/Rat/Racer/MainActivity.java
+++ pRatRace/app/src/main/java/Rat/Racer/MainActivity.java
@@ -51,7 +51,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate/show the menu; this adds items to the action bar if it is present.
+        // you can add more items into the menu at res/menu/main.xml
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
@@ -59,6 +60,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        //here we reference our main fragment res/layout/content_main.xml with .id
+        //which has a navGraph to a nav schema res/navigaton/mobile_nav...
+        //which in turn supplies fragment info with refs to .../layout/[frag.xml]
+        //and somehow navigates to frags w/ below code
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
